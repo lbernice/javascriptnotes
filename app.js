7bc6339eb907f91c1b7fde1c921f5b3e05f10f22
lbernice <bernicel677@gmail.com>
2023-10-02 13:11:45 -0400

    commit

---
+++ app.js
@@ -0,0 +1,22 @@
+//function is input output device 
+//we declare, define, and run functions
+//functions can accept multiple arguments using, 
+// function test(message){
+//     console.log(message);
+// }
+// //String "" sentences, word etc.
+// test("Ugh Ruth");
+// //Numbers/integers
+// test(45);
+// //Booleans true or false
+// test(true);
+// declare variables, define variables
+const num1 = Number(prompt("enter a number"));
+const num2 = Number(prompt("enter a number"));
+
+function add(x,y){
+    console.log(x + y);
+}
+add(num1, num2);
+
+
